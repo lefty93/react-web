9d2a0bf1360f945dcd243b678f73e8984e387511
Lefty93 <yongchuseng@gmail.com>
2023-03-15 06:51:56 +0800

    Work on Rooms page

--- src/components/pages/Rooms.js
+++ src/components/pages/Rooms.js
@@ -1,6 +1,9 @@
+// TODO: Add some rooms using card components, or create new card components. Refer RoxyHotel https://roxyhotel.my/sematan/ 
+
 import React from 'react'
 import '../../App.css'
 import Footer from '../Footer'
+
 
 function Rooms() {
     return (
@@ -9,7 +12,16 @@ function Rooms() {
                 <h2>OUR ROOMS</h2>
                 <p>IN RAIA HOTEL KUCHING</p>
                 <p className='rooms-content'>Spaciously designed 237 guestrooms and suites, furnished with a choice of King size or twin bedded, full air-conditioned, LCD TV with satellite news and entertainment channels. Complimentary airport shuttle provided for the convenience of our guest.</p>
+                <div className="rooms-container">
+                    <div>Twin ROOM</div>
+                    <div>Twin ROOM</div>
+                    <div>Twin ROOM</div>
+                    <div>Twin ROOM</div>
+                    <div>Twin ROOM</div>
+                    <div>Twin ROOM</div>
+                </div>
             </div>
+            
             <Footer />
         </>
 
@@ -18,4 +30,3 @@ function Rooms() {
 
 export default Rooms
 
-// TODO: Add some rooms using card components 
